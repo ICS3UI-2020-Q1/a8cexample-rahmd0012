bf718c31c54f65f53fc62c28ee26f1375fbcbd98
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-30 13:51:58 +0000

    GUI Layout managers example

--- Main.java
+++ Main.java
@@ -4,22 +4,123 @@ import java.awt.event.*;
 
 public class Main implements Runnable, ActionListener{
 
-  // Class Variables  
+  // Class Variables 
+  JPanel mainPanel;
+  JPanel topPanel;
+  JPanel buttonPanel;
+
+  JButton[] numberButtons;
+  JButton addButton;
+  JButton subtractButton;
+  JButton mulButton;
+  JButton divButton;
+  JButton equalButton;
+  JButton decimalButton;
+
+  JTextField display; 
+
+  Font biggerFont;
   
 
 
   // Method to assemble our GUI
   public void run(){
     // Creats a JFrame that is 800 pixels by 600 pixels, and closes when you click on the X
-    JFrame frame = new JFrame("Title");
+    JFrame frame = new JFrame("Calculator");
     // Makes the X button close the program
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // makes the windows 800 pixel wide by 600 pixels tall
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
- 
+
+    // initialize the main panel with a border layout
+    mainPanel = new JPanel();
+    mainPanel.setLayout(new BorderLayout());
+
+    // initialize the button panel with a grid layout
+    buttonPanel = new JPanel();
+    buttonPanel.setLayout(new GridLayout(4,4));
+
+    // initialize the top panel with the default layout (flow)
+    topPanel = new JPanel();
+
+    // initialize a bigger font
+    biggerFont = new Font("arial", Font.PLAIN, 24);
+
+    // create the display
+    display = new JTextField("0");
+    // set a preferred size so flow layout knows it should be bigger
+    display.setPreferredSize(new Dimension(800, 100));
+    // add it to the top
+    topPanel.add(display);
+
+    // make the display font bigger
+    display.setFont(biggerFont);
+
+    // creating the number buttons 
+    numberButtons = new JButton[10]; // sets the size of the array
+    for(int i = 0; i < numberButtons.length; i++){
+      numberButtons[i] = new JButton("" + i);
+      // set the action command
+      numberButtons[i].setActionCommand("" + i);
+      // add ActionListener
+      numberButtons[i].addActionListener(this);
+    }
+
+    // create the operation buttons 
+    equalButton = new JButton("=");
+    equalButton.addActionListener(this);
+    equalButton.setActionCommand("equal");
+
+    addButton = new JButton("+");
+    addButton.addActionListener(this);
+    addButton.setActionCommand("add");
+
+    subtractButton = new JButton("-");
+    subtractButton.addActionListener(this);
+    subtractButton.setActionCommand("subtract");
+
+    mulButton = new JButton("X");
+    mulButton.addActionListener(this);
+    mulButton.setActionCommand("multiply");
+
+    divButton = new JButton("/");
+    divButton.addActionListener(this);
+    divButton.setActionCommand("divide");
+
+    decimalButton = new JButton(".");
+    decimalButton.addActionListener(this);
+    decimalButton.setActionCommand("decimal");
+
+    // add the buttons to the grid
+    buttonPanel.add(numberButtons[7]);
+    buttonPanel.add(numberButtons[8]);
+    buttonPanel.add(numberButtons[9]);
+    buttonPanel.add(divButton);
+
+    buttonPanel.add(numberButtons[4]);
+    buttonPanel.add(numberButtons[5]);
+    buttonPanel.add(numberButtons[6]);
+    buttonPanel.add(mulButton);
+
+    buttonPanel.add(numberButtons[1]);
+    buttonPanel.add(numberButtons[2]);
+    buttonPanel.add(numberButtons[3]);
+    buttonPanel.add(subtractButton);
+
+    buttonPanel.add(numberButtons[0]);
+    buttonPanel.add(decimalButton);
+    buttonPanel.add(addButton);
+    buttonPanel.add(equalButton);
+
+    // assemble the panels 
+    mainPanel.add(topPanel, BorderLayout.PAGE_START);
+    mainPanel.add(buttonPanel, BorderLayout.CENTER);
     
+    // add the maine panel to the frame
+    frame.add(mainPanel);
+
 
   }
 
